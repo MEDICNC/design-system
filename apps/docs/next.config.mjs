c997a7bc749c44c81dac0176fd51f0fb5217cc46
jaemu <shimwoan@gmail.com>
2024-06-04 10:44:07 +0900

    Uikit build 에러 해결 try 6 (output export, search param 이슈)

--- apps/docs/next.config.mjs
+++ apps/docs/next.config.mjs
@@ -3,6 +3,7 @@ import { build } from "velite";
 /** @type {import('next').NextConfig} */
 export default {
   // othor next config here...
+  output: "export",
   webpack: (config) => {
     config.plugins.push(new VeliteWebpackPlugin());
     return config;
